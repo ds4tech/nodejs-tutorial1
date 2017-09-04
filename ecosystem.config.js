8c4cb06663a21cca4fd5b2bc182e8bad3cae50d6
Ubuntu <ds4tech@gmail.com>
2017-09-04 14:02:45 +0000

    corrected ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,5 @@
 
-module.export = {
+module.exports = {
 	apps: [{
 		name: 'tut1',
 		script: './index.js'
@@ -7,10 +7,10 @@ module.export = {
 	deploy: {
 		production: {
 			user: 'ubuntu',
-			host: 'ec2-34-250-72-252.eu-west-1.compute.amazonaws.com',
-			key: '~/.ssh/id_rsa.pub',
+			host: 'ec2-54-72-63-100.eu-west-1.compute.amazonaws.com',
+			key: '~/.ssh/github_rsa',
 			ref: 'origin/master',
-			repo: 'git@github.com:ds4tech/tut1-nodejs.git'
+			repo: 'git@github.com:ds4tech/tut1-nodejs.git',
 			path: '/home/ubuntu/server/tut1-nodejs',
 			'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
 		}
